6c3991a08d60dc0ca09095c7b3be5fa1caa5a9d5
Hemachander64 <83521019+Hemachander64@users.noreply.github.com>
2025-03-19 02:41:01 -0500

    mixin mixinted
    The MobGoal behavior has been updated with mixin to have mobs both attack the player if the player is within a certain range, and to ignore the player and attack the base if not. This functionality is still untested

--- src/main/java/bunguspacks/invasionslib/mobbehaviors/MobGoal.java
+++ src/main/java/bunguspacks/invasionslib/mobbehaviors/MobGoal.java
@@ -1,35 +1,32 @@
 package bunguspacks.invasionslib.mobbehaviors;
 
-import net.minecraft.MinecraftVersion;
-import net.minecraft.entity.Entity;
+import bunguspacks.invasionslib.mixin.MobEntityAccessor;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.TargetPredicate;
-import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.ai.goal.GoalSelector;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
-import java.util.ArrayList;
+public class MobGoal<T extends PathAwareEntity> extends Goal {
 
-public class MobGoal<T extends MobEntity> extends Goal {
-
-    MobEntity entity;
+    PathAwareEntity entity;
     int dementiaTolerance = 20;
     int dementiaTimer = 0;
     int targetRange = 0;
+    GoalSelector goalSelector;
+    ActiveTargetGoal<PlayerEntity> fightPlayer;
+    //Ideally range is infinite, but i am going to set a "reasonable number" in its place
+    GoToWalkTargetGoal attackBase = new GoToWalkTargetGoal((PathAwareEntity)this.entity, this.entity.speed);
 
-    public MobGoal(MobEntity entity){
+    public MobGoal(PathAwareEntity entity){
         this.entity = entity;
     }
 
-    public MobGoal(MobEntity entity, int targetRange){
+    public MobGoal(PathAwareEntity entity, int targetRange){
         this.entity = entity;
         this.targetRange = targetRange;
     }
@@ -42,7 +39,9 @@ public class MobGoal<T extends MobEntity> extends Goal {
     @Override
     public void start(){
         super.start();
-
+        this.goalSelector = ((MobEntityAccessor)entity).getGoalSelector();
+        //Mob should always have attacking base as priority 1
+        goalSelector.add(1, attackBase);
     }
 
     @Override
@@ -54,12 +53,16 @@ public class MobGoal<T extends MobEntity> extends Goal {
     @Override
     public void tick(){
         super.tick();
+        //The Following code is entirely to handle the mob's targetting of the player
         //If closest player is in targetting range of mob and not null, set target and start dementia timer
         LivingEntity closestPlayer = this.entity.getWorld().getClosestEntity(PlayerEntity.class, TargetPredicate.DEFAULT, null, entity.getX(), entity.getY(), entity.getZ(), new Box(entity.getBlockPos()).expand(targetRange));
         if (!(closestPlayer == null)){
-            this.entity.setTarget(closestPlayer);
             dementiaTimer = dementiaTolerance;
-            entity.clearPositionTarget();
+            entity.setTarget(closestPlayer);
+            if (!(goalSelector.getGoals().contains(fightPlayer))) {
+                fightPlayer = new ActiveTargetGoal<PlayerEntity>(this.entity, PlayerEntity.class, true, true);
+                goalSelector.add(1, fightPlayer);
+            }
         }
         //If not forgor, target should still be set to the previous player
         else {
@@ -67,10 +70,13 @@ public class MobGoal<T extends MobEntity> extends Goal {
             //If forgor
             if (!(dementiaTimer == 0)) {
                 //target beacon thing
+                if (goalSelector.getGoals().contains(fightPlayer)) {
+                    goalSelector.remove(fightPlayer);
+                }
+                goalSelector.remove(fightPlayer);
                 entity.setPositionTarget(new BlockPos(0,0,0), 1);
             }
         }
-
     }
 
     @Override
