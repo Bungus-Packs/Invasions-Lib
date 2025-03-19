75da4c48fe589bf55dedafe22c44211e5a1a6f52
democat <cwongmath@gmail.com>
2025-03-18 22:14:58 -0500

    Initial commit

---
+++ src/main/java/bunguspacks/invasionslib/InvasionsLib.java
@@ -0,0 +1,10 @@
+package org.example.invasionslib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InvasionsLib implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/bunguspacks/invasionslib/client/InvasionsLibClient.java
@@ -0,0 +1,10 @@
+package org.example.invasionslib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InvasionsLibClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
